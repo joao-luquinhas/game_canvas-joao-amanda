ccf444cd68e9cd1dd8aa25e2b3a7ac4785f01cce
joao-luquinhas <joao.anjos19@etec.sp.gov.br>
2023-09-14 20:47:16 -0300

    atualizando projeto

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# game_canvas-joao-amanda
+## Descrição:
+Este é um projeto de jogo no canvas, que tem como objetivo, representar de forma ilustrativa a música:
+
+- [Legião Urbana - Tempo Perdido](https://www.youtube.com/watch?v=2hr7Uqu6G80)
+
+Ela retratam temas como determinação, coragem, reforçam sempre como não devemos perder nosso tempo
+com coisas fúteis ou desnecessárias e sempre evitar conflitos sem motivo. 
+O jogo retrata esse temas de forma simplificada e tenta fazer o usuário refletir sobre estes temas ao jogar o jogo.
+Jogo tem como objetivo representar o clássico jogo de ping-pong, onde o objetivo é ter uma pontuação, maior que a de seu adversário antes do tempo acabar, para vencer o jogo, divirta-se!
--- js/form.js
+++ js/form.js
@@ -3,17 +3,23 @@ const ctx = canvas.getContext("2d"); //largura: 1100px e altura: 300px 550 150
 
 //posição inical das barras
 let RetPequeno = 120; 
-let RetGrande = 95;
 let RetPC = 95;
+
+//variavel para os pontos
+let pontos_play = document.getElementById('placar_player');
+let pontos_PC = document.getElementById('placar_PC');
 
 //variaveis do elemento bola
 let bola_x = 550;
 let bola_y = 150;
-let vel_x = 2;
-let vel_y = 2;
+let vel_x = 4;
+let vel_y = 3;
 
+//velocidade da barra do computador
 let vel_PC = 2;
 
+//variável que indica que o jogoestá rodando
+let partida = true;
 
 
 //função do botão jogar
@@ -25,135 +31,148 @@ function jogar(){
 
 
 
-function bola() {
-    ctx.clearRect(175, 0, 825, 300);
+//começa correr o tempo após apertar o botão
+const timer = (duration, display) => {
+    let timer = duration;
+    let minutes, seconds;
 
-    ctx.beginPath();
-    ctx.fillStyle = "red";
-    ctx.arc(bola_x, bola_y, 5, 0, Math.PI * 2);
-    ctx.fill();
+    let interval = setInterval(() => {
+        minutes = Math.floor(timer / 60);
+        seconds = timer % 60;
 
-    // Atualiza a posição da bola com base na velocidade
-    bola_x += vel_x;
-    bola_y += vel_y;
+        minutes = minutes < 10 ? '0' + minutes : minutes;
+        seconds = seconds < 10 ? '0' + seconds : seconds;
 
-    // Verifica se a bola atingiu a borda superior ou inferior do canvas e inverte a direção vertical
-    if (bola_y < 0 || bola_y > 300) {
-        vel_y = -vel_y;
-    }
+        display.innerHTML = `${minutes}:${seconds}`;
 
-    //pontuação 
-    if (bola_x<0){
-        PC_ponto();
-    }
-    if (bola_x>1100){
-        player_ponto();
-    }
-
-    // Verifica se a bola atingiu a barra do jogador ou do PC e inverte a direção horizontal
-    if (
-        (bola_x < 45 && bola_x > 35 && bola_y > RetPequeno && bola_y < RetPequeno + 80) ||
-        (bola_x > 1100 - 15 && bola_x < 1100 - 5 && bola_y > RetPC && bola_y < RetPC + 120) ||
-        (bola_x < 175 && bola_x + 5 > 165 && bola_y > RetGrande && bola_y < RetGrande + 120)
-    ) {
-        vel_x = -vel_x;
-    }
-
-
-    // Verifica se a bola saiu do canvas (ponto marcado) e redefine a posição
-    if (bola_x < 0 || bola_x > 1100) {
-        bola_x = 550;
-        bola_y = 150;
-        vel_x = -vel_x;
-        vel_y = Math.random() > 0.5 ? 2 : -2; // Redefine a velocidade vertical da bola aleatoriamente
-    }
-
-    // Solicita uma nova animação
-    requestAnimationFrame(jogar);
+        if (timer <= 0) {
+            partida = false; //partida acaba quando tempo zera
+                if(pontos_PC>pontos_play){
+                    ctx.clearRect(0,0,1100,300);
+                    alert("Computador Venceu o Jogo!");
+                } else if(pontos_PC<pontos_play){
+                    ctx.clearRect(0,0,1100,300);
+                    alert("Você Venceu Parabéns");
+                } else{
+                    ctx.clearRect(0,0,1100,300);
+                    alert("Empate");
+                }
+            clearInterval(interval);
+        } else {
+            timer -= 1;
+        }
+    }, 1000);
 }
+window.onload = () => {
+    const display = document.getElementById('timer');
+    const startButton = document.getElementById('jogar');
 
+    startButton.addEventListener('click', () => {
+        const duration = 0.2 * 60; // 3 minutos em segundos
+        timer(duration, display);
+    });
+};
+
+
+
+function bola() {
+    if(partida){
+        ctx.clearRect(175, 0, 825, 300);
+
+        ctx.beginPath();
+        ctx.fillStyle = "red";
+        ctx.arc(bola_x, bola_y, 7, 0, Math.PI * 2);
+        ctx.fill();
+
+        // Atualiza a posição da bola com base na velocidade
+        bola_x += vel_x;
+        bola_y += vel_y;
+
+        // Verifica se a bola atingiu a borda superior ou inferior do canvas e inverte a direção vertical
+        if (bola_y < 0 || bola_y > 300) {
+            vel_y = -vel_y;
+        }
+
+        //pontuação 
+        if (bola_x<0){
+            PC_ponto();
+        }
+        if (bola_x>1100){
+            player_ponto();
+        }
+
+        // Verifica se a bola atingiu a barra do jogador ou do PC e inverte a direção horizontal
+        if (
+            (bola_x < 45 && bola_x > 35 && bola_y > RetPequeno && bola_y < RetPequeno + 80) ||
+            (bola_x > 1100 - 15 && bola_x < 1100 - 5 && bola_y > RetPC && bola_y < RetPC + 120) 
+        ) {
+            vel_x = -vel_x;
+        }
+
+
+        // Verifica se a bola saiu do canvas (ponto marcado) e redefine a posição
+        if (bola_x < 0 || bola_x > 1100) {
+            bola_x = 550;
+            bola_y = 150;
+            vel_x = -vel_x;
+            vel_y = Math.random() > 0.5 ? 2 : -2; // Redefine a velocidade vertical da bola aleatoriamente
+        }
+
+        // Solicita uma nova animação
+        requestAnimationFrame(jogar);
+    }
+}
 
 
 function player_ponto(){
-    let pontos = document.getElementById('placar_player');
-    let ganhou
-    ganhou = Number(pontos.textContent) + 1;
-    pontos.textContent = ganhou;
+    let ganhou_play
+    ganhou_play = Number(pontos_play.textContent) + 1;
+    pontos_play.textContent = ganhou_play;
 }
 
 function PC_ponto(){
-    let pontos = document.getElementById('placar_PC');
-    let ganhou
-    ganhou = Number(pontos.textContent) + 1;
-    pontos.textContent = ganhou;
+    let ganhou_PC
+    ganhou_PC = Number(pontos_PC.textContent) + 1;
+    pontos_PC.textContent = ganhou_PC;
 }
 
 
 
 function barra_PC(){
-    ctx.clearRect(1000, 0, 1100, 300); //apaga o canvas para redesenhar a barra
+    if(partida){
+        ctx.clearRect(1000, 0, 1100, 300); //apaga o canvas para redesenhar a barra
 
-    ctx.beginPath();
-    ctx.fillStyle = "white";
-    ctx.fillRect(1000,RetPC,10,120);//180
-    
-    if (bola_y < RetPC) {
-        RetPC -= vel_PC; // Move a barra do PC para cima
-    } else if (bola_y > RetPC + 120) {
-        RetPC += vel_PC; // Move a barra do PC para baixo
-    }
+        ctx.beginPath();
+        ctx.fillStyle = "white";
+        ctx.fillRect(1000,RetPC,10,120);//180
+        
+        if (bola_y < RetPC) {
+            RetPC -= vel_PC; // Move a barra do PC para cima
+        } else if (bola_y > RetPC + 120) {
+            RetPC += vel_PC; // Move a barra do PC para baixo
+        }
 
-    // Certifique-se de que a barra do PC não ultrapasse os limites do canvas
-    if (RetPC < 0) {
-        RetPC = 0;
-    } else if (RetPC + 120 > 300) {
-        RetPC = 300 - 120;
+        //barra do PC não ultrapasse os limites do canvas
+        if (RetPC < 0) {
+            RetPC = 0;
+        } else if (RetPC + 120 > 300) {
+            RetPC = 300 - 120;
+        }
     }
 }
 
-
-
-
-
-
 function barra_play(){
-    ctx.clearRect(0, 0, 175, 300); //apaga o canvas para redesenhar a barra
+    if(partida){
+        ctx.clearRect(0, 0, 175, 300); //apaga o canvas para redesenhar a barra
 
-    ctx.beginPath();
-    ctx.fillStyle = "white";
-    ctx.fillRect(35,RetPequeno,10,80);//0 //220
-
-    ctx.beginPath();
-    ctx.fillStyle = "white";
-    ctx.fillRect(165,RetGrande,10,120);//180
+        ctx.beginPath();
+        ctx.fillStyle = "white";
+        ctx.fillRect(35,RetPequeno,10,80);//0 //220
+    }
 }
 
 document.addEventListener("keydown", function(event) {
     if (event.key === "w" || event.key === "W") {
-
-        RetGrande = RetGrande-2;
-        barra_play();
-
-        if(RetGrande<0){
-            RetGrande = 0;
-        }
-    }
-});
-
-document.addEventListener("keydown", function(event) {
-    if (event.key === "s" || event.key === "S") {
-
-        RetGrande = RetGrande+2;
-        barra_play();
-
-        if(RetGrande>180){
-            RetGrande = 180;
-        }
-    }
-});
-
-document.addEventListener("keydown", function(event) {
-    if (event.key === "q" || event.key === "Q") {
 
         RetPequeno = RetPequeno-4;
         barra_play();
@@ -165,7 +184,7 @@ document.addEventListener("keydown", function(event) {
 });
 
 document.addEventListener("keydown", function(event) {
-    if (event.key === "a" || event.key === "A") {
+    if (event.key === "s" || event.key === "S") {
         
         RetPequeno = RetPequeno+4;
         barra_play();
@@ -225,5 +244,7 @@ ctx2.lineTo(55, 52);
 ctx2.fillStyle = "#daa520";
 ctx2.fill();
 ctx2.stroke();
+
+
 
 
